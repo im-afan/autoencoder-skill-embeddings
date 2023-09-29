66e7148b994bf06040db42462edac42ccb150ae3
Andrew Fan <andrewkongfan@gmail.com>
2023-09-28 18:58:52 -0700

    .

--- code/train_lowlevel.py
+++ code/train_lowlevel.py
@@ -1,6 +1,13 @@
 import sys
 from rl_zoo3.train import train
+import gymnasium as gym
+from custom_envs.ant_turn_pybullet import AntTargetPosBulletEnv
 
-sys.argv = ["python", "--algo", "ppo", "--env", "AntBulletEnv-v0"]
+gym.register(
+    id="AntTargetPosBulletEnv-v0",
+    entry_point=AntTargetPosBulletEnv
+)
+sys.argv = ["python", "--algo", "ppo", "--env", "AntTargetPosBulletEnv-v0", "--eval-freq", "100"]
+#sys.argv = ["python", "--algo", "ppo", "--env", "AntBulletEnv-v0"]
 
 train()
