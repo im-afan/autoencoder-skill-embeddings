8a317c29f147bbdf156ed49ea53e1ca70a3a1434
Andrew Fan <andrewkongfan@gmail.com>
2023-10-11 18:00:11 -0700

    successfully made agent that can do the obstacle thinggit add .git add .

--- code/train_highlevel.py
+++ code/train_highlevel.py
@@ -6,9 +6,9 @@ import sys
 register_envs()
 
 #sys.argv = ["python", "--algo", "ppo", "--env", "AntTargetPosHighLevel-v0", "--eval-freq", "100000", "--tensorboard-log", "/tmp/stable-baselines3", "-P"]
-#sys.argv = ["python", "--algo", "ppo", "--env", "AntObstacleHighLevel-v0", "--eval-freq", "100000", "--tensorboard-log", "/tmp/stable-baselines3", "-P"]
+sys.argv = ["python", "--algo", "ppo", "--env", "AntObstacleHighLevel-v0", "--eval-freq", "100000", "--tensorboard-log", "/tmp/stable-baselines3", "-P"]
 #sys.argv = ["python", "--algo", "ppo", "--env", "AntBulletEnv-v0"]
-sys.argv = ["python", "--algo", "ppo", "--env", "AntObstacleHighLevel-v0", "--eval-freq", "100000", "-i", "logs/ppo/AntObstacleHighLevel-v0_4/AntObstacleHighLevel-v0.zip", "--tensorboard-log", "/tmp/stable-baselines3", "-P"]
+#sys.argv = ["python", "--algo", "ppo", "--env", "AntObstacleHighLevel-v0", "--eval-freq", "100000", "-i", "logs/ppo/AntObstacleHighLevel-v0_4/AntObstacleHighLevel-v0.zip", "--tensorboard-log", "/tmp/stable-baselines3", "-P"]
 #sys.argv = ["python", "--algo", "ppo", "--env", "AntObstacleHighLevel-v0", "--eval-freq", "100000", "--tensorboard-log", "/tmp/stable-baselines3", "-P"]
 
 train()
