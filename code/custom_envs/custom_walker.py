69b83b1d44f2dd90e79bb859cdf9a2dd2749f05b
Andrew Fan <andrewkongfan@gmail.com>
2023-10-26 09:45:58 -0700

    have trial

--- code/custom_envs/custom_walker.py
+++ code/custom_envs/custom_walker.py
@@ -61,9 +61,9 @@ class WalkerTargetPosBulletEnv(
 
         self.max_ep_length = 1000
         if(self.logging):
-            self.max_ep_length = 150 
+            self.max_ep_length = 150
 
-        MJCFBaseBulletEnv.__init__(self, robot, render)
+        MJCFBaseBulletEnv.__init__(self, robot, render, render_mode)
         self.observation_space = self.observation_space
         self.action_space = self.action_space
 
