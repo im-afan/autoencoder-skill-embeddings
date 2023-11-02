c4b4f6e91b0ad7df10859ff25492cabd0b5779fe
Andrew Fan <andrewkongfan@gmail.com>
2023-11-01 21:54:09 -0700

    OMG HAVE 2ND TRIAL ASKLDFJnaskldfjn

--- code/test_highlevel.py
+++ code/test_highlevel.py
@@ -10,11 +10,14 @@ from register_envs import register_envs
 register_envs()
 
 wanted_env = sys.argv[1]
+test_path = sys.argv[2]
+sys.argv.pop()
+sys.argv.pop()
 
 with open("cur_path.txt", "w") as f:
-    f.write("trials/0")
+    f.write(test_path)
 #sys.argv = ["python", "--algo", "ppo", "--env", "AntTargetPosHighLevel-v0", "--folder", "logs/", "-n", "10000"]
-sys.argv = ["python", "--algo", "ppo", "--env", wanted_env + "HighLevel-v0", "--folder", "trials/0/agents", "-n", "10000"]
+sys.argv = ["python", "--algo", "ppo", "--env", wanted_env + "HighLevel-v0", "--folder", test_path + "/agents/", "-n", "10000"]
 #sys.argv = ["python", "--algo", "ppo", "--env", "HumanoidTargetPosHighLevel-v0", "--folder", "logs/", "-n", "10000"]
 #sys.argv = ["python", "--algo", "ppo", "--env", "AntBulletEnv-v0"]
 
